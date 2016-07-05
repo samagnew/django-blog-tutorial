c172ef89f9afb93b250ec983b47f7f8d4bc2c860
samagnew <samagnew@protonmail.com>
2016-07-06 00:24:08 +0300

    First commit of django blog tutorial code

---
+++ djtut/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/Data/Dropbox/Dropbox/Documents/Code/python/virtenv/django-tut/djtut/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
